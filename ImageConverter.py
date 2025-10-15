143b6481c85513737089e61f124b8cdb3be8aa8a
roninbrandsma <brandsmaronin57@gmail.com>
2025-10-15 20:57:24 +0100

    created tkinter window and added file selection

--- ImageConverter.py
+++ ImageConverter.py
@@ -1,5 +1,25 @@
-
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
 from PIL import Image
 
-im = Image.open().convert("RGB")
-im.save()
+root = Tk()
+root.title("Image Converter")
+root.minsize(800, 400)
+
+mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+
+filePath = filedialog.askopenfilename(
+    title="Select a file",
+    filetypes=[("Images", "*.png *.jpg *.jpeg"), ("All Files", "*.*")]
+)
+print("Selected file:", filePath)
+
+files = filedialog.askopenfilenames(
+    title="Select Multiple Files",
+    filetypes=[("Image Files", "*.jpg;*.png"), ("All Files", "*.*")]
+)
+print("Selected files:", files)
+
+root.mainloop()
