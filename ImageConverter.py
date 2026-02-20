9fc55fa52bfae887b6232072c8571d0f8da148a7
roninbrandsma <brandsmaronin57@gmail.com>
2026-02-20 00:34:31 +0000

    can now convert files to png

--- ImageConverter.py
+++ ImageConverter.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+import numpy as np
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
@@ -11,7 +13,6 @@ window.minsize(800, 400)
 mainframe = ttk.Frame()
 mainframe.grid(column=0, row=0, sticky=NSEW)
 
-fileString = []
 fileNames = []
 
 def browseFiles():
@@ -29,10 +30,19 @@ def browseFiles():
     label.config(text="\n".join(fileNames))
     print(fileNames)
 
+
+def OnSelection():
+    selectedOption = dropdownVar.get()
+
+    if selectedOption == ".png":
+        convertToPNG()
+    elif selectedOption == ".jpeg":
+        convertToJPEG()
+
 def convertToJPEG():
     for file in fileNames:
         im = Image.open(file)
-        rgbIm = im.convert("RGB")
+        rgbImg = im.convert("RGB")
 
         f, e = os.path.splitext(file)
         if e == ".jpeg":
@@ -40,13 +50,32 @@ def convertToJPEG():
 
         name = f + ".jpeg"
         print(name)
-        rgbIm.save(name)
+        rgbImg.save(name)
 
+def convertToPNG():
+    for file in fileNames:
+        im = Image.open(file)
+        rgbaImg = im.convert("RGBA")
+
+        f, e = os.path.splitext(file)
+        if e == ".png":
+            print("Can not convert PNG file to PNG.")
+
+        name = f + ".png"
+        print(name)
+        rgbaImg.save(name)
+
+
+options = ["Select a Type:", ".png", ".jpeg"]
+dropdownVar = StringVar()
+dropdownVar.set(options[0])
 
 # Widgets
 label = ttk.Label(window, text="")
 fileSearchBtn = ttk.Button(window, text="Select File...", command=browseFiles)
-convertBtn = ttk.Button(window, text="Convert", command=convertToJPEG)
+convertBtn = ttk.Button(window, text="Convert", command=OnSelection)
+dropdownMenu = ttk.OptionMenu(window, dropdownVar, *options)
+
 
 # Columns
 window.columnconfigure(0, weight=1)
@@ -59,10 +88,12 @@ window.columnconfigure(4, weight=1)
 window.rowconfigure(0, weight=1)
 window.rowconfigure(1, weight=3)
 window.rowconfigure(2, weight=1)
-
+window.rowconfigure(3, weight=1)
 # Widget Position
+
 label.grid(column=0, row=0)
-fileSearchBtn.grid(column=3, row=2)
-convertBtn.grid(column=4, row=2)
+fileSearchBtn.grid(column=3, row=3)
+dropdownMenu.grid(column=4, row=2)
+convertBtn.grid(column=4, row=3)
 
 window.mainloop()
